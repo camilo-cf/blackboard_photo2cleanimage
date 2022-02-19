81cb4c0038f3a2bc7c8ae9bc23563c1d35ba9b0f
Camilo Cáceres <camilokceres@gmail.com>
2022-02-19 17:03:41 -0500

    Added and updated: Setup configuration

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ COPY . /app
 WORKDIR /app
 ARG DEBIAN_FRONTEND=noninteractive
 RUN apt-get update && apt-get install python3-pip -y
-RUN pip3 install -r ./requirements.txt  && rm -rf /root/.cache
+RUN python3 ./setup.py install
 RUN apt-get autoremove -y gcc
 EXPOSE 80
 CMD uvicorn src.app:app --host 0.0.0.0 --port 80
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 
 from setuptools import setup, find_packages
 
-requirements = [ ]
-
-test_requirements = [ ]
+file_requirements = open('requirements.txt', 'r')
+requirements = file_requirements.readlines()
+requirements = [line.replace("\n", "") for line in requirements] 
 
 setup(
     author="Camilo Caceres F",
@@ -20,7 +20,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
     ],
-    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    description="Image API converter from blackboard to B/W.",
     install_requires=requirements,
     license="GNU General Public License v3",
     include_package_data=True,
@@ -28,7 +28,6 @@ setup(
     name='blackboard_photo2cleanimage',
     packages=find_packages(include=['blackboard_photo2cleanimage', 'blackboard_photo2cleanimage.*']),
     test_suite='tests',
-    tests_require=test_requirements,
     url='https://github.com/camilo-cf/blackboard_photo2cleanimage',
     version='0.1.0',
     zip_safe=False,
